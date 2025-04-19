47a7cccf5b908daee67681c5cbc9d0124efd991c
dendendenden123 <159616363+dendendenden123@users.noreply.github.com>
2025-04-19 17:36:01 +0800

    Refactor code structure and optimize performance across multiple modules

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\TestController;
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('test', [TestController::class, 'test']);;
+Route::get('test', [TestController::class, 'test']);
+
+Route::get('{view}', function ($view) {
+   
+    return view('welcome');
+});
+
+// C:\xampp\htdocs\Alumni_Tracer_System\resources\views\template\mazer\dist\index.blade.php
