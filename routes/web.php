54c1d5f625e10dc50f0a19fe9c623e693d5ac204
dendendenden123 <159616363+dendendenden123@users.noreply.github.com>
2025-04-19 08:46:11 +0800

    Refactor code structure and optimize performance across multiple modules

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\TestController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', [TestController::class, 'test']);;
